60ef2c3abcd6377407dc10a2c772029561827261
themescaline <lex.korovin@gmail.com>
2019-08-13 09:41:10 +0300

    Init commit

---
+++ src/main/java/ru/vtb/insurance/ApplicationConfiguration.java
@@ -0,0 +1,8 @@
+package ru.vtb.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class ApplicationConfiguration extends SpringApplication {
+}
