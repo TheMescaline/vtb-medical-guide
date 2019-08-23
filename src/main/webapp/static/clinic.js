91f2e629ea453cf787c4f0571ba8f90099bcc54e
themescaline <lex.korovin@gmail.com>
2019-08-23 14:41:34 +0300

    fix adaptive web for smaller devices

--- src/main/webapp/static/clinic.js
+++ src/main/webapp/static/clinic.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('#example').DataTable( {
         ajax: {
+            // url: "http://localhost:8080/api/v1/clinics/",
             url: "https://vtb-medical-guide.herokuapp.com/api/v1/clinics/",
             dataSrc: '_embedded.clinicList'
         },
